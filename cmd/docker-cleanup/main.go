3bd256edd2d5478ab07f42e646a15ecc22d0d4b4
Pieter Claerhout <pieter@yellowduck.be>
2020-05-27 07:55:55 +0200

    Updated the readme

--- cmd/docker-cleanup/main.go
+++ cmd/docker-cleanup/main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"strings"
+	"time"
 
-	"github.com/pieterclaerhout/docker-cleanup/versioninfo"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/pieterclaerhout/go-formatter"
+	"github.com/pieterclaerhout/go-log"
 )
 
 func main() {
-	fmt.Println("Project: "+ versioninfo.ProjectName)
-	fmt.Println("Description: "+ versioninfo.ProjectDescription)
-	fmt.Println("Copyright: "+ versioninfo.ProjectCopyright)
-	fmt.Println("Version: "+ versioninfo.Version)
-	fmt.Println("Revision: " + versioninfo.Revision)
-	fmt.Println("Branch: " + versioninfo.Branch)
+
+	log.PrintColors = true
+	log.PrintTimestamp = false
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		All: true,
+	})
+	log.CheckError(err)
+
+	for _, container := range containers {
+
+		if container.State == "running" {
+			continue
+		}
+
+		containerID := container.ID[:10]
+
+		log.Info("> Removing container:", containerID, container.Image)
+
+	}
+
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{
+		All: false,
+	})
+
+	for _, image := range images {
+		for _, tag := range image.RepoTags {
+
+			tagParts := strings.SplitN(tag, ":", 2)
+
+			log.Infof(
+				"%-40s | %-30s | %s | %s | %10s",
+				tagParts[0],
+				tagParts[1],
+				image.ID[7:19],
+				time.Unix(image.Created, 0),
+				formatter.FileSize(image.Size),
+			)
+		}
+	}
+
+	// log.InfoDump(images, "images")
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/pieterclaerhout/docker-cleanup
 
 go 1.14
+
+require (
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pieterclaerhout/go-formatter v1.0.4
+	github.com/pieterclaerhout/go-log v1.14.0
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
+)
